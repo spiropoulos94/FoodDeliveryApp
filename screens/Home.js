98f89a95c238343a5101f2a90ce75db0b9604eac
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-05-25 23:32:50 +0300

    finished home screen

--- screens/Home.js
+++ screens/Home.js
@@ -22,7 +22,7 @@ import {
 
 //43:30
 
-const Home = () => {
+const Home = ({ navigation }) => {
   const [categories, setCategories] = useState(categoryData);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [restaurants, setRestaurants] = useState(restaurantData);
@@ -160,10 +160,139 @@ const Home = () => {
     );
   };
 
+  const renderRestaurantList = () => {
+    const getCategoryNameByID = (id) => {
+      let category = categories.filter((a) => a.id == id);
+      if (category.length > 0) {
+        return category[0].name;
+      }
+      return "";
+    };
+
+    const renderItem = ({ item }) => {
+      return (
+        <TouchableOpacity
+          style={{ marginBottom: SIZES.padding * 2 }}
+          onPress={() =>
+            navigation.navigate("Restaurant", {
+              item,
+              currentLocation,
+            })
+          }
+        >
+          <View style={{ marginBottom: SIZES.padding }}>
+            <Image
+              source={item.photo}
+              resizeMode="cover"
+              style={{
+                width: "100%",
+                height: 200,
+                borderRadius: SIZES.radius,
+              }}
+            />
+            <View
+              style={{
+                position: "absolute",
+                bottom: 0,
+                height: 50,
+                width: SIZES.width * 0.3,
+                backgroundColor: COLORS.white,
+                borderTopRightRadius: SIZES.radius,
+                borderBottomLeftRadius: SIZES.radius,
+                alignItems: "center",
+                justifyContent: "center",
+                ...styles.shadow,
+              }}
+            >
+              <Text>{item.duration}</Text>
+            </View>
+          </View>
+          {/* Restaurant Info */}
+          <Text style={{ ...FONTS.body2 }}>{item.name}</Text>
+          <View
+            style={{
+              marginTop: SIZES.padding,
+              flexDirection: "row",
+            }}
+          >
+            <Image
+              source={icons.star}
+              style={{
+                height: 20,
+                width: 20,
+                tintColor: "primary",
+                marginRight: 10,
+              }}
+            />
+            <Text style={{ ...FONTS.body3 }}>{item.rating}</Text>
+            {/* Categories */}
+            <View
+              style={{
+                flexDirection: "row",
+                marginLeft: 10,
+              }}
+            >
+              {item.categories.map((categoryId) => {
+                return (
+                  <View style={{ flexDirection: "row" }} key={categoryId}>
+                    <Text
+                      style={{
+                        ...FONTS.body3,
+                      }}
+                    >
+                      {getCategoryNameByID(categoryId)}
+                    </Text>
+                    <Text
+                      style={{
+                        ...FONTS.h3,
+                        color: COLORS.darkgray,
+                      }}
+                    >
+                      {" "}
+                      .{" "}
+                    </Text>
+                  </View>
+                );
+              })}
+              {/* Price */}
+              {[1, 2, 3].map((priceRating) => (
+                <Text
+                  key={priceRating}
+                  style={{
+                    ...FONTS.body3,
+                    color:
+                      priceRating <= item.priceRating
+                        ? COLORS.black
+                        : COLORS.darkgray,
+                  }}
+                >
+                  $
+                </Text>
+              ))}
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <FlatList
+        data={restaurants}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={renderItem}
+        contentContainerStyle={{
+          paddingHorizontal: SIZES.padding * 2,
+          paddingBottom: 30,
+        }}
+      />
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderMainCategories()}
+      {renderRestaurantList()}
     </SafeAreaView>
   );
 };
--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+
+//58:00
 
 const Restaurant = () => {
   return (
